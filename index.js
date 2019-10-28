7a6ecafeba666fd13e8498bea8c446f04641154e
Taylor Lacerte <taylormlacerte@gmail.com>
2019-10-28 16:27:48 -0500

    refresh when collides

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const canvas = document.querySelector('canvas')
+const score = document.getElementById('score')
 canvas.width = 300
 canvas.height = 300
 
@@ -20,6 +21,19 @@ function step() {
         //blocking square
     context.strokeRect(baseX, baseY, 50, 50)
         //takes function as an arg to refresh page
+
+    //let sum = 0
+    if (playerX >= baseX &&
+        playerY >= baseY &&
+        playerX <= baseX + 50 &&
+        playerY <= baseY + 50) {
+        baseX = Math.floor(Math.random() * 250)
+        baseY = Math.floor(Math.random() * 250)
+            //sum += 1
+            //console.log(sum)
+    }
+    //score.innerHTML = sum
+
     requestAnimationFrame(step)
 }
 requestAnimationFrame(step)
